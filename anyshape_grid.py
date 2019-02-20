4f990d8f36f05f590a685d2077fc9530a06fa3fb
Brendan Retter <br293@exeter.ac.uk>
2019-02-20 16:56:37 +0000

    Merge with test branch. Removed kfunc2 which was redundant.

--- anyshape_grid.py
+++ anyshape_grid.py
@@ -378,53 +378,6 @@ def kfunc(x,y,t,yso_map=None,grid=None,opti=False,diag=False):
     else:
         return K, L
 
-def kfunc2(x,y,t,yso_map=None,grid=None,opti=False,diag=False):
-    """
-    Calculates K function for points with coords x,y.
-    Most likely x and y are the positions of yso.
-    """
-
-    if yso_map is None:
-        yso_map = yso_to_grid(np.array([x,y]),grid)
-        
-    if grid is None:
-        grid = coverage
-
-    #yso will each count themselves once over the course of the 
-    #algorithm. This accounts for the self-counting.
-    yso_sum = -np.sum(yso_map)
-    yso_sum = []
-    area_sum = []
-
-    #Generate relative circle coords for approx centre of map
-    shape = np.shape(yso_map)
-    x_mid,y_mid = ij2xy(shape[0]/2,shape[1]/2)
-    mid_coords = circle(x_mid,y_mid,t,np.ones(shape),relative=True)
-    
-    for i in range(len(x)):
-        xg,yg = xy2grid(x[i],y[i])
-        Lx = 2*delDist2Grid(t,axis='x')
-        Ly = 2*delDist2Grid(t,axis='y')
-        if opti == True and box_check(xg,yg,Lx,Ly,grid=grid):
-            coords = np.copy(mid_coords)+np.array([xg,yg]).reshape(2,1)
-        else:
-            coords = circle(x[i],y[i],t,grid)
-                
-        n_coords = np.shape(coords)[1]
-        area_sum.append(n_coords)
-        yso_sum.append([-1])
-        for j in range(n_coords):
-            yso_sum[i]+=yso_map[coords[0,j],coords[1,j]]
-    area = float(dx)*dy*np.array(area_sum)
-    yso_sum = np.array(yso_sum).reshape(len(x))
-    lmda = np.sum(yso_map)/float(np.sum(grid)*dx*dy)
-    K = np.pi*t**2/(lmda*len(x))*(np.sum(yso_sum/area))
-    L = np.sqrt(K/np.pi) - t
-    if diag == True:
-        return K,L,np.sum(yso_sum),np.sum(area)
-    else:
-        return K, L
-
 def Oring(x,y,t,w,yso_map=None,grid=None,opti=False,diag=False):
     """
     Calculates Oring function for points with coords x,y.
