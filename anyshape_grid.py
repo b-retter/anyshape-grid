866fb474e665032cbc2398294cacccf134414162
Brendan Retter <br293@exeter.ac.uk>
2019-06-18 15:39:46 +0100

    Serpens Core

--- anyshape_grid.py
+++ anyshape_grid.py
@@ -1193,7 +1193,7 @@ Coverage map.
 #fits_path = '/Users/bretter/Documents/StarFormation/SFR_data'
 #fits_path = '../SFR_data'
 fits_path = '.'
-fits_name = 'SERAQU_IRAC1234M1_cov.fits'
+fits_name = 'SER_IRAC1234M1_cov.fits'
 coverage,header = fits.getdata(os.path.join(fits_path,fits_name), header=True)
 w_obj = wcs.WCS(header)
 
@@ -1215,7 +1215,7 @@ else:
 ##Extracting sections of map
 #desired sector of sky bottom-left and top-right.
 
-bounds = np.array([[277.2,277.7],[-2.25,-1.75]])
+bounds = np.array([[277.4,277.6],[1.18,1.28]])
 
 #number of processes
 noProcess = 1
@@ -1259,7 +1259,7 @@ pos_data = data[:,:2]
 pos_mask = (pos_data[:,0] > bounds[0,0]) & (pos_data[:,0] < bounds[0,1]) & (pos_data[:,1] > bounds[1,0]) & (pos_data[:,1] < bounds[1,1])
 
 
-region = 'serpens_south'
+region = 'serpens_core'
 fpath = '{:s}/'.format(region)
 class_list = ['classI0','flat','classII','classIII','all']
 #loop over each yso class
@@ -1316,7 +1316,7 @@ for a in range(4,5):
     
     #Get stats
     steps = 20
-    r = np.linspace(0.01,0.5,steps)
+    r = np.linspace(0.001,0.05,steps)
     w = r*0.6
 
     results = np.empty((2,steps))
