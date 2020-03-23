dc4ae16f63aa1e7ad1eff1e5e83fa04482e51a52
Brendan Retter <brendan.retter@gmail.com>
2020-03-23 14:16:34 +0000

    Updated all_regions_envelopes to use pc steps instead of angle steps.

--- all_regions_envelopes.py
+++ all_regions_envelopes.py
@@ -51,6 +51,10 @@ all_r = np.stack([np.linspace(0.25/steps,0.25,steps),
                       np.linspace(0.5/steps,0.5,steps),
                       np.linspace(0.3/steps,0.3,steps)],axis=0)
 
+#Prep for finding r steps in terms of parsecs
+max_r_values = np.array([0.25,0.05,0.7,0.5,0.3])
+pc_step = 0.03
+
 for j,region in enumerate(allregions):
     print('{:s}'.format(region))
     fits_name = allfits_names[j]
@@ -81,10 +85,15 @@ for j,region in enumerate(allregions):
         w_obj.grid[zeroes] = 0
 
     prob_map = ext_obj.grid**2.052
-    
-    r = all_r[j]
+
     distance_to = alldistances[j]
 
+    #Recalculate radial distances by converting to parsecs, finding the steps in parsecs
+    #and converting them back into angles.
+    conversion_factor = distance_to*(np.pi/180)
+    max_r_pc = max_r_values[j]*conversion_factor
+    r_pc = np.arange(pc_step,max_r_pc,pc_step)
+    r = r_pc/conversion_factor
     
     ##Initialise envelope
     #loop over each yso class
@@ -100,7 +109,7 @@ for j,region in enumerate(allregions):
         
         results = allenv(val,r,w,LOOPS,w_obj,density=prob_map,mode='nhpp',noP=noProcesses,timer=False)
 
-        statsfile='{:s}_{:s}_beta2.052_envelope'.format(region,cl)
+        statsfile='{:s}_{:s}_beta2.052_0.03pc_envelope'.format(region,cl)
         statsdir = os.path.join(fpath,region,statsfile)
         np.save(statsdir,results)
 
